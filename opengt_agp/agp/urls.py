59358e54b393954cc0d898e4a5c1fce3ca6a1c6f
Denis Klimov <zver@altlinux.org>
2010-03-28 19:20:06 +0600

    remove needless now index view

--- opengt_agp/agp/urls.py
+++ opengt_agp/agp/urls.py
@@ -4,7 +4,6 @@ from agp.views import *
 
 
 urlpatterns = patterns('',
-	url(r'^$', index, name="index"),
 	url(r'^map/$', map, name="map"),
 	url(r'^login/$', login, name="login"),
 	url(r'^logout/$', logout, name="logout"),
